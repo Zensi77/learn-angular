08fa4191e4a62ea8ab259739c22878bd3f564b02
Juanma <juanmaespi7@gmail.com>
2024-10-20 18:25:12 +0200

    sync

---
+++ 06-formsApp/src/app/reative/reative.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReativeRoutingModule } from './reative-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReativeRoutingModule
+  ]
+})
+export class ReativeModule { }
