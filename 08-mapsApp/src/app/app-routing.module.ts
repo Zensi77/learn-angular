1bc09979c5f0512f61a1d95d69b1e9985ea25608
Zensi77 <juanmaespi7@gmail.com>
2024-10-28 20:53:39 +0100

    Seccion 22- Standalone Components

--- 08-mapsApp/src/app/app-routing.module.ts
+++ 08-mapsApp/src/app/app-routing.module.ts
@@ -1,19 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: 'maps',
-    loadChildren: () => import('./maps/maps.module').then( m => m.MapsModule ),
+    path: "alone",
+    loadComponent: () =>
+      import("./alone/alone-page/alone-page.component").then(
+        (m) => m.AlonePageComponent
+      ),
   },
   {
-    path: '**',
-    redirectTo: 'maps',
-  }
+    path: "maps",
+    loadChildren: () => import("./maps/maps.module").then((m) => m.MapsModule),
+  },
+  {
+    path: "**",
+    redirectTo: "maps",
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
